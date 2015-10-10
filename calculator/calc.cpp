0667f1974965c57c0270c13e5291dfac6b648652
nathansgreen <nathansgreen@users.noreply.github.com>
2015-10-10 14:08:59 -0500

    C++ calculator.

---
+++ calculator/calc.cpp
@@ -0,0 +1,43 @@
+// Copyright 2015 Nathan Green
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::setprecision;
+
+int main(void) {
+    float arg1, arg2;
+    char oper;
+    cout << "Enter a formula: ";
+    std::cin >> arg1 >> oper >> arg2;
+    float rslt;
+    switch (oper) {
+    case '+':
+        rslt = arg1 + arg2;
+        break;
+    case '-':
+        rslt = arg1 - arg2;
+        break;
+    case '*':
+        rslt = arg1 * arg2;
+        break;
+    case '/':
+        if (arg2 == 0.0) {
+            cout << "ERRDIV0" << std::endl;
+            return 2;
+        }
+        rslt  = arg1 / arg2;
+        break;
+    case '^':
+        rslt = pow(arg1, arg2);
+        break;
+    case '%':
+        rslt = arg1 % arg2;
+        break;
+    default:
+        cout << "BADOP" << oper << std::endl;
+        return 1;
+    }
+    cout << arg1 << ' ' << oper << ' ' << arg2
+        << " = " << rslt << std::endl;
+}
