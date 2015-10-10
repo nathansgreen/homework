3f2e78d7440fc87ebf446d1562b5bad94f5b0b72
nathansgreen <nathansgreen@users.noreply.github.com>
2015-10-10 10:06:07 -0500

    Re-implemented Java code using streams and lambdas for a big performance boost.

--- fizzbuzz/Fizzbuzz.java
+++ fizzbuzz/Fizzbuzz.java
@@ -1,5 +1,42 @@
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Spliterator;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
 public class Fizzbuzz {
+  private static final byte[] fizz = "fizz".getBytes(US_ASCII);
+  private static final byte[] buzz = "buzz".getBytes(US_ASCII);
+  private static final int[] range = new int[100];
+  static {
+    for (int i = 0; i < range.length; i++)
+      range[i] = i + 1;
+  }
+
   public static void main(String... args) {
+    final byte[] s = new byte[10];
+    final int pos = 0;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream(10);
+    //for (int j = 0; j < 100000; j++)
+    IntStream.of(range)
+      .forEach(i -> {
+        try {
+          int a = i % 3, b = i % 5;
+          if (a == 0) { out.write(fizz); }
+          if (b == 0) { out.write(buzz); }
+          if (a != 0 && b != 0) {
+            out.write(Integer.toString(i).getBytes(US_ASCII));
+          }
+          out.write('\n');
+          out.writeTo(System.out);
+          out.reset();
+        } catch (IOException e) {}
+      });
+  }
+
+  static void fizzbuzz() {
     StringBuilder s = new StringBuilder(10);
     //for (int j = 0; j < 100000; j++)
     for (int i = 1; i < 101; i++) {
--- fizzbuzz/perf.md
+++ fizzbuzz/perf.md
@@ -14,9 +14,10 @@ perl 5.18 |  5.83 |  5.67 |  0.05
 rust      |  6.39 |  3.18 |  3.20
 dmd       |  7.05 |  7.02 |  0.02
 ruby 2.2  |  8.41 |  7.82 |  0.23
+java 8 (8u60) <sup>[3](#note3)</sup> | 9.09 | 5.78 user | 3.48
 python27  | 12.37 | 12.31 |  0.04
 C# (mono 4.2) | 13.49 | 9.61 | 3.74
-java8     | 18.75 | 12.01 |  6.95
+java 5 (8u60) | 18.75 | 12.01 |  6.95
 bash 3.2 <sup>[2](#note2)</sup> | 624.00 | 550.50 | 51.00
 fish 2.2 <sup>[1](#note1)</sup> | 93,050.00 | 41,150.00 | 55,730.00
 
@@ -24,3 +25,5 @@ fish 2.2 <sup>[1](#note1)</sup> | 93,050.00 | 41,150.00 | 55,730.00
 <a name="note1"/>[1] Fish shell runs so slow that I was forced to extrapolate from 100 runs. Being 3-4 orders of magnitude slower than optimal is definitely painful.
 
 <a name="note2"/>[2] Bash was tested with 10,000 runs and extrapolated upward.
+
+<a name="note3"/>[3] The Java 8 version uses streams and lambdas, and is still slower than it could be due to use of `Integer.toString()`.
