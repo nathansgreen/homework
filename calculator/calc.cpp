4a15abb4eb9cf7b68e1749c718bc2d13fe9ae9c6
nathansgreen <nathansgreen@users.noreply.github.com>
2015-10-13 23:36:04 -0500

    Factorial

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -12,16 +12,24 @@ union Rslt {
     Rslt(float ff) { f = ff; }
 };
 
+float factorial(float val, int next) {
+    if (next <= 1) return val;
+    val *= next;
+    return factorial(val, --next);
+}
+
 int main(void) {
     float arg1, arg2;
     char oper;
     cout << "Enter a formula: ";
-    std::cin >> arg1 >> oper >> arg2;
+    std::cin >> arg1 >> oper;
+    if (oper != '!') std::cin >> arg2;
     Rslt rslt;
     rslt = oper == '+' ? arg1 + arg2 : rslt;
     rslt = oper == '-' ? arg1 - arg2 : rslt;
     rslt = oper == '*' ? arg1 * arg2 : rslt;
     rslt = oper == '^' ? pow(arg1, arg2) : rslt;
+    rslt = oper == '!' ? factorial(arg1, arg1 - 1): rslt;
     rslt = oper == '%' ? static_cast<int>(arg1) % static_cast<int>(arg2) : rslt;
     if (oper == '/' && arg2 == 0.0) {
         cout << "ERRDIV0" << std::endl;
@@ -32,6 +40,7 @@ int main(void) {
         cout << "BADOP" << oper << std::endl;
         return 1;
     }
-    cout << arg1 << ' ' << oper << ' ' << arg2
-        << " = " << rslt.f << std::endl;
+    cout << arg1 << ' ' << oper;
+    if (oper != '!') cout << ' ' << arg2;
+    cout << " = " << rslt.f << std::endl;
 }
