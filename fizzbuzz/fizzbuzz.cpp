2b9f368f6377476ff8d4a171c7f52b3ca812ff21
nathansgreen <nathansgreen@users.noreply.github.com>
2015-10-13 23:38:02 -0500

    Minor speed boost to c++ fizzbuzz implementation.

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -5,8 +5,11 @@ using std::string;
 using std::cout;
 
 int main(void) {
+    // running only one thread, so don't lock
+    std::ios_base::sync_with_stdio(false);
     string s = string(10, ' ');
     s.clear();
+    //for (int j = 0; j < 100000; j++)
     for (int i = 1; i < 101; i++) {
         if (i % 3 == 0) s.append("Fizz");
         if (i % 5 == 0) s.append("Buzz");
